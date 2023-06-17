c3899dd1c5179345a9d9584b5b91d32b52395fd5
alexmlamb <lambalex@iro.umontreal.ca>
2023-06-16 18:17:18 -0700

    Updates

---
+++ data/data.py
@@ -0,0 +1,42 @@
+from tqdm import tqdm
+import numpy as np
+from room_polygon_obstacle_env import RoomPolygonObstacleEnv
+import pickle
+
+def generate_data(env, num_data_samples, dataset_path):
+    X = []
+    A = []
+    ast = []
+    est = []
+    done = []
+
+    for _ in tqdm(range(0, num_data_samples)):
+        a = env.random_action()
+
+        x, agent_state, exo_state = env.get_obs()
+        env.step(a)
+
+        A.append(a[:])
+        X.append(x[:])
+        ast.append(agent_state[:])
+        est.append(exo_state[:])
+        done.append(False)
+
+    X = np.asarray(X).astype('float32')
+    A = np.asarray(A).astype('float32')
+    ast = np.array(ast).astype('float32')
+    est = np.array(est).astype('float32')
+    done = np.array(done).astype('float32')
+
+    pickle.dump({'X': X, 'A': A, 'ast': ast, 'est': est, 'done': done},
+                open(dataset_path, 'wb'))
+
+    print(f'data generated and stored in {dataset_path}')
+
+
+if __name__ == "__main__":
+
+    env = RoomPolygonObstacleEnv()
+
+    generate_data(env, 500000, 'dataset.p')
+
